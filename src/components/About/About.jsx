5bb09cf3ccc77050f6916f5ffa0656bff13c7d13
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-16 15:29:01 +0000

    js

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/work_logo/lokeshphoto.jpg';
+import profileImage from '../../assets/work_logo/profile.jpg';
 
 const About = () => {
   return (
